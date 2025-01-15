9f1c4ed24e7b1503afb21d38e485b1323a149177
Tim Spinney <tspinney@seattleu.edu>
2025-01-15 12:31:53 -0800

    replaced placeholder in AddToCart with quantity input and "add to cart" button

--- lib/add_to_cart.dart
+++ lib/add_to_cart.dart
@@ -5,6 +5,22 @@ class AddToCart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        /* TextFormField would normally want to fill the width of its parent,
+           but Row wants to determine its width based on its children. To resolve
+           this deadlock, we use Flexible to negotiate between the two. SizedBox
+           would also work, but fixed sizes don't always mesh well with accessibility
+           options like text scaling set in the OS.
+         */
+        Flexible(
+          child: TextFormField(),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Add to cart'),
+        ),
+      ],
+    );
   }
 }
