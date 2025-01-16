bb244fcc73a65c6af55bcc97ceb6681aaedeaf41
Tim Spinney <tspinney@seattleu.edu>
2025-01-15 16:43:03 -0800

    added AppBar to product details page so users without a physical "back" button can get back to the product list

--- lib/product_detail_page.dart
+++ lib/product_detail_page.dart
@@ -11,6 +11,7 @@ class ProductDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: SafeArea(
         child: SingleChildScrollView(
           child: ProductDetails(product: product),
