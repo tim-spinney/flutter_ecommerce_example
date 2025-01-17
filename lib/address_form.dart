ec137fb01f277a3434fd6b76c354ef9047bfacf4
Tim Spinney <tspinney@seattleu.edu>
2025-01-16 18:08:01 -0800

    added form and example of validating fields

--- lib/address_form.dart
+++ lib/address_form.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'model/address.dart';
+
 class AddressForm extends StatefulWidget {
   const AddressForm({super.key});
 
@@ -8,6 +10,7 @@ class AddressForm extends StatefulWidget {
 }
 
 class _AddressFormState extends State<AddressForm> {
+  final _formKey = GlobalKey<FormState>();
   final _line1Controller = TextEditingController();
   final _line2Controller = TextEditingController();
 
@@ -18,47 +21,66 @@ class _AddressFormState extends State<AddressForm> {
   bool _isPrimaryAddress = false;
 
   _onSaveAddress() {
-
+    if(!_formKey.currentState!.validate()) {
+      return;
+    }
+    final address = Address(
+      _line1Controller.text,
+      _line2Controller.text,
+      _cityController.text,
+      '',
+      _zipCodeController.text,
+    );
+    _formKey.currentState!.reset();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextFormField(
-          decoration: const InputDecoration(labelText: 'Address line 1'),
-          controller: _line1Controller,
-        ), // line 1
-        TextFormField(
-          decoration: const InputDecoration(labelText: 'Address line 2'),
-          controller: _line2Controller,
-        ), // line 2
-        TextFormField(
-          decoration: const InputDecoration(labelText: 'City'),
-          controller: _cityController,
-        ), // city
-        TextFormField(
-          decoration: const InputDecoration(labelText: 'State'),
-        ), // state
-        TextFormField(
-          decoration: const InputDecoration(labelText: 'ZIP code'),
-          controller: _zipCodeController,
-        ), // zip
-        Checkbox(
-          value: _isPrimaryAddress,
-          onChanged: (newValue) {
-            if(newValue != null) {
-              setState(() {
-                _isPrimaryAddress = newValue;
-              });
-            }
-          },
-        ),
-        ElevatedButton(
-          child: const Text('Save'),
-          onPressed: _onSaveAddress,
-        ),
-      ],
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'Address line 1'),
+            controller: _line1Controller,
+            validator: (input) {
+              if(input == null || input.isEmpty) {
+                return 'Line 1 cannot be empty.';
+              }
+              return null;
+            },
+          ), // line 1
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'Address line 2'),
+            controller: _line2Controller,
+          ), // line 2
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'City'),
+            controller: _cityController,
+          ), // city
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'State'),
+          ), // state
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'ZIP code'),
+            controller: _zipCodeController,
+          ), // zip
+          Checkbox(
+            value: _isPrimaryAddress,
+            onChanged: (newValue) {
+              if(newValue != null) {
+                setState(() {
+                  _isPrimaryAddress = newValue;
+                });
+              }
+            },
+          ),
+          ElevatedButton(
+            child: const Text('Save'),
+            onPressed: _onSaveAddress,
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/model/address.dart
@@ -0,0 +1,10 @@
+
+class Address {
+  final String line1;
+  final String line2;
+  final String city;
+  final String state;
+  final String zipCode;
+
+  Address(this.line1, this.line2, this.city, this.state, this.zipCode);
+}
